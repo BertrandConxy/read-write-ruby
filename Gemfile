4984cdaa3b6072d5e4485f4de8f65fbadd3bf9a4
bertrand <mutanganabertrand@gmail.com>
2022-10-07 20:57:46 +0200

    Finish the app

--- Gemfile
+++ Gemfile
@@ -1,2 +1,2 @@
 source 'https://rubygems.org'
-gem 'pdftotext'
+gem 'pdf-reader'
--- app.rb
+++ app.rb
@@ -1,16 +1,27 @@
-require 'pdftotext'
 require_relative './generate_pdf'
+require 'pdf/reader'
 
 def app
-    file_path = './HEADING.pdf'
-    text_file = Pdftotext.text(file_path)
-    input = gets.chomp
-    file = File.open(text_file)
+    puts "Enter pdf file path"
+    pdf_input = gets.chomp
+
+    puts "Enter grouping keyword"
+    search_input = gets.chomp
+
+    reader = PDF::Reader.new(pdf_input)
+
+    new_file = File.new("file.txt", "w")
+
+    reader.pages.each do |page|
+        File.write(new_file, "#{page.text}.", mode: "a+")
+    end
+
+    file = File.open(new_file)
     file_data = file.readlines.map(&:chomp)
     $string  = ''
     file_data.each_with_index do |line, index|
-        if line.include?(input)
-            if $string.length > 0 &&  $string.include?(input)
+        if line.include?(search_input)
+            if $string.length > 0 &&  $string.include?(search_input)
                 generate_pdf = GeneratePdf.new
                 generate_pdf.generate($string, index)
                 $string = ''
